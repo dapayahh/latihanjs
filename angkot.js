14e4ef3e041f10aea8030fd0fb2653b636a69ca7
dafapratama <dafapratama.muharam@gmail.com>
2025-01-12 22:30:13 +0700

    latihan2

---
+++ angkot.js
@@ -0,0 +1,14 @@
+var jlhAngkot
+
+var angkot = 1;
+
+while(angkot <=jlhAngkot) {
+if (angkot >6) {
+
+}
+    console.log ('Angkot No. '+ angkot +
+         ' beroperasi dengan baik');
+
+    angkot++;
+}
+
